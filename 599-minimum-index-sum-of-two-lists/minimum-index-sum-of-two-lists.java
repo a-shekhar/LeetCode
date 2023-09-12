c89de5d26f1be54366b50cfb469fcb2a38e078a7
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2023-09-13 01:00:46 +0530

    Time: 9 ms (57.64%) | Memory: 44.4 MB (67.50%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.java
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.java
@@ -1,29 +1,29 @@
 class Solution {
     public String[] findRestaurant(String[] list1, String[] list2) {
-        List<Integer> indexList = new ArrayList<>();
-        Map<String, Integer> map = new HashMap<>();
-        List<String> wordList = new ArrayList<>();
-
-        for(String word : list1){
-            wordList.add(word);
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        int minval = Integer.MAX_VALUE;
+        int temp = 0;
+        
+         for(int i = 0; i < list1.length; i++){
+            map1.put(list1[i], i);
         }
 
         for(int i = 0; i < list2.length; i++){
-            if(wordList.contains(list2[i])){
-                int temp = i + wordList.indexOf(list2[i]);
-                map.put(list2[i], temp);
-                indexList.add(temp);
+            if(map1.containsKey(list2[i])){
+                temp = i + map1.get(list2[i]);
+                map2.put(list2[i], temp);
+                minval = Math.min(minval, temp);
             }
         }
 
-        Collections.sort(indexList);
-        int minVal = indexList.get(0);
-        List<String> ans = new ArrayList<>();
-        for(Map.Entry<String, Integer> entry: map.entrySet()){
-            if(entry.getValue() == minVal){
-                ans.add(entry.getKey());
+        
+        for(Map.Entry<String, Integer> entry: map2.entrySet()){
+            if(entry.getValue() == minval){
+                list.add(entry.getKey());
             }
         }
-        return ans.stream().toArray(String[] ::new);
+        return list.toArray(new String[list.size()]);
     }
 }
