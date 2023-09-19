a218ae3a6ec3d4e5d45ceda25bec9c3a132e4445
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2023-09-20 00:41:10 +0530

    Time: 10 ms (16.04%), Space: 40.8 MB (45.16%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        List<Character> list = new ArrayList<>();
+        for(int i=0; i<t.length(); i++){
+            list.add(t.charAt(i));
+        }
+
+        for(int i=0; i<s.length(); i++){
+            list.remove(Character.valueOf(s.charAt(i)));
+        }
+
+        return list.get(0);
+    }
+}
