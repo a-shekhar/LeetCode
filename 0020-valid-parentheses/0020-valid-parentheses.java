663e30b23f47caa01038230839cb3d93a9253abe
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2023-09-29 14:13:28 +0530

    Time: 2 ms (82.27%), Space: 40.6 MB (41.77%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (!stack.isEmpty()) {
+                if (stack.peek() == '(' && s.charAt(i) == ')') {
+                    stack.pop();
+                } else if (stack.peek() == '{' && s.charAt(i) == '}') {
+                    stack.pop();
+                } else if (stack.peek() == '[' && s.charAt(i) == ']') {
+                    stack.pop();
+                } else {
+                    stack.push(s.charAt(i));
+                }
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        return stack.isEmpty();
+    }
+}
