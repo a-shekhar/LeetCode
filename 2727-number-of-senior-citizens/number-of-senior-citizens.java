d515a7064243d6ac014adaa91b2c6855504fa784
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2024-08-01 23:09:45 +0530

    Time: 1 ms (82.64%) | Memory: 42.6 MB (45.62%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int count = 0;
+        for(String detail: details){
+            String age = detail.substring(11, 13);
+            if(Integer.valueOf(age) > 60){
+                count++;
+            }
+        }
+        return count;
+    }
+}
