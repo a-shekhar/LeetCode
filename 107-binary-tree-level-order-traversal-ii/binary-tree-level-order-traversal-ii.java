5edd2ef30e1232bc306dad9f6770973f96a5704c
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2024-09-22 16:43:17 +0530

    Time: 1 ms (93.47%) | Memory: 42.8 MB (53.45%) - LeetSync

--- 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
@@ -15,34 +15,30 @@
  */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        List<List<Integer>> reserveLevelOrder = new ArrayList<>();
-        if(root == null){
-            return reserveLevelOrder;
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) {
+            return result;
         }
-        List<Integer> levelOrder;
-        Stack<List<Integer>> stack = new Stack<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
         int size;
-        TreeNode popped;
-        while(!queue.isEmpty()){
-            levelOrder = new ArrayList<>();
+        TreeNode currentNode;
+        List<Integer> levelOrder = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
             size = queue.size();
-            for(int i = 0; i < size; i++){
-                popped = queue.poll();
-                levelOrder.add(popped.val);
-                if(popped.left!= null){
-                    queue.offer(popped.left);
+            levelOrder = new ArrayList<>();
+            for(int i = 0; i < size; i++) {
+                currentNode = queue.poll();
+                levelOrder.add(currentNode.val);
+                if (currentNode.left != null) {
+                    queue.add(currentNode.left);
                 }
-                if(popped.right!= null){
-                    queue.offer(popped.right);
+                if (currentNode.right != null) {
+                    queue.add(currentNode.right);
                 }
             }
-            stack.push(levelOrder);
+            result.add(0, levelOrder);
         }
-        while(!stack.isEmpty()){
-            reserveLevelOrder.add(stack.pop());
-        }
-        return reserveLevelOrder;
+        return result;
     }
 }
