78a829c6886aa41e6d98d9d8a5663380d1883c76
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-07-12 21:29:32 +0530

    Time: 20 ms (15.65%), Space: 48.6 MB (17.42%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,18 +1,30 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
+       
         for(String str: strs){
-            char[] chars = str.toCharArray();
-            Arrays.sort(chars);
-            String sortedString = new String(chars);
-            if(!map.containsKey(sortedString)){
-                map.put(sortedString, new ArrayList<>());
+            int[] intArr = new int[26];
+            for(char ch : str.toCharArray()){
+                intArr[ch - 97]++;
             }
 
-            List<String> value = map.get(sortedString);
+            StringBuilder builder = new StringBuilder();
+            for(int i : intArr){
+                builder.append(i);
+                builder.append("*"); // just for seperation and correct hash, can be any char
+            }
+
+            String hashedString = builder.toString();
+
+            if(!map.containsKey(hashedString)){
+                map.put(hashedString, new ArrayList<>());
+            }
+
+            List<String> value = map.get(hashedString);
             value.add(str);
-            map.put(sortedString, value);
+            map.put(hashedString, value);
         }
+
         List<List<String>> result = new ArrayList<>();
 
         for(List<String> list : map.values()){
