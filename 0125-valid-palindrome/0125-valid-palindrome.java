a9950fe95803fc28ab81511334f3d6be364901b6
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-07-13 21:51:50 +0530

    Time: 2 ms (98.48%), Space: 42.8 MB (90.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,27 @@
 class Solution {
-     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
-        StringBuilder builder = new StringBuilder();
-        char ch;
-        for(int i = 0; i<s.length(); i++){
-            ch = s.charAt(i);
-            if(Character.isAlphabetic(ch) || Character.isDigit(ch)){
-                builder.append(ch);
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i < j){
+            if(!Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+                continue;
+            } 
+            
+            if(!Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+                continue;
             }
 
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
+                return false;
+            }
+
+            i++;
+            j--;
 
         }
-        String str = builder.toString();
-        String reverse = builder.reverse().toString();
-        return str.equals(reverse);
+        return true;
     }
 }
