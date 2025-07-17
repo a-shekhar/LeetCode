5faf120bb4902977695081b507cc88aeaeb1027f
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-07-17 11:02:03 +0530

    Time: 1 ms (16.58%), Space: 44.2 MB (88.88%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -10,19 +10,24 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null){
+        public boolean hasCycle(ListNode head) {
+
+        if(head == null){
             return false;
         }
-        ListNode slow  = head;
+
+        ListNode slow = head;
         ListNode fast = head.next;
-        while(slow != null && fast != null && fast.next != null){
+
+        while(fast != null && fast.next != null && fast.next.next != null){
             if(slow == fast){
                 return true;
             }
+
             slow = slow.next;
             fast = fast.next.next;
         }
+
         return false;
     }
 }
