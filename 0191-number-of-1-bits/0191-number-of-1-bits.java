7663cc3af5ade613ce10be803a68595bbfad1682
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-07-25 14:36:29 +0530

    Time: 0 ms (100%), Space: 40.8 MB (55.75%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,7 +1,7 @@
 class Solution {
     public int hammingWeight(int n) {
        int count = 0;
-        while(n > 0){
+        while(n != 0){
             count += n & 1; //compare right most bit 
             n = n >>> 1; // unsigned shift bit by 1 
         }
