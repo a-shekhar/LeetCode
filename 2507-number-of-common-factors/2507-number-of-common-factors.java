52a7fd83fc9b436f1fda644fa0070847498c090f
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-08-06 22:24:30 +0530

    Time: 0 ms (100%), Space: 40.9 MB (6.68%) - LeetHub

--- 2507-number-of-common-factors/2507-number-of-common-factors.java
+++ 2507-number-of-common-factors/2507-number-of-common-factors.java
@@ -1,12 +1,20 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        int n = Math.min(a, b);
-        int count = 1;
+        int n = gcd(a, b);
+        int count = 1; // 1 always divides
         for(int i = 2; i <= n; i++){
             if(a % i == 0 && b % i == 0){
                 count++;
             }
         }
-        return count;
+
+        return count; 
+    }
+
+    public int gcd(int a, int b) {
+        if(b == 0){
+            return a;
+        }
+        return gcd(b, a % b);
     }
 }
