0576c54a7bfef2ef0714492c24e7debd9c71891c
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-09-12 15:02:56 +0530

    Time: 29 ms (4.82%) | Memory: 48.3 MB (5.37%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,36 +1,38 @@
 class MinStack {
-    
-    Stack<Integer> stack;
+
+     Stack<Integer> stack;
     Stack<Integer> minStack;
 
     public MinStack() {
         stack = new Stack<>();
-        minStack = new Stack<>();
+        minStack = new Stack<>();    
     }
-
+    
     public void push(int val) {
         stack.push(val);
-        if(!minStack.empty()){
-            if(minStack.peek() >= val){
-                minStack.push(val);
-            }
-        }else{
+        if(minStack.isEmpty() || (!minStack.isEmpty() && minStack.peek() >= val)){
             minStack.push(val);
         }
     }
-
+    
     public void pop() {
-        if(stack.pop().intValue() == minStack.peek().intValue()){
-            minStack.pop();
-        }
+       if(!stack.isEmpty()){
+            if(stack.peek().equals(minStack.peek())){
+                stack.pop();
+                minStack.pop();
+            }else{
+                stack.pop();
+            }
+       }
     }
-
+    
     public int top() {
-        return stack.peek();
+        return stack.isEmpty() ? -1 : stack.peek();   
     }
-
+    
     public int getMin() {
-        return minStack.peek();
+        System.out.println(minStack);
+        return minStack.isEmpty() ? -1: minStack.peek();
     }
 }
 
