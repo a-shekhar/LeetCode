5a048d21973964511eb99e1d7d27232dac0f61b6
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-09 11:39:58 +0530

    Time: 24 ms (86.49%), Space: 60.1 MB (70.9%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,22 +1,24 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-         int[] result = new int[temperatures.length];
+         Deque<Integer> stack = new ArrayDeque<>(); // will store  index here
+        int n = temperatures.length;
+        int[] result = new int[n];
 
-        Stack<Integer> stack = new Stack<>();
+        for(int i = n - 1; i >= 0; i--){
+            if(!stack.isEmpty()){
+                while(!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]){
+                    stack.pop();
+                }
+            } 
 
-          for (int i = temperatures.length - 1; i >= 0; i--) {
-            // Pop indices with temperatures <= current temperature
-            while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
-                stack.pop();
+            if(stack.isEmpty()){
+                result[i] = 0;
+            } else {
+                result[i] = stack.peek() - i;
             }
-
-            // If the stack is not empty, the top is the next warmer day
-            result[i] = stack.isEmpty() ? 0 : stack.peek() - i;
-
-            // Push current index to stack
+            
             stack.push(i);
         }
-
         return result;
     }
 }
