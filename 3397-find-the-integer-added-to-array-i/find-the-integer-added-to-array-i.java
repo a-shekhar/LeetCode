36ea602a103640e11726df32a44c8954ed7db68c
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-13 00:27:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (89.23%) - LeetSync

---
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        int sum = 0;
+        for(int num : nums2){
+            sum += num;
+        }
+
+        for(int num : nums1){
+            sum -= num;
+        }
+
+        return sum/nums1.length;
+    }
+}
