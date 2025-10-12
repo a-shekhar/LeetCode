19ec10bd1c3bfa013277645495a40d702971266e
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-13 00:40:45 +0530

    Time: 0 ms (100%), Space: 44.2 MB (69.73%) - LeetHub

---
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int sum = 0;
+        for(int num : nums){
+            if(num < 10){
+                sum += num;
+            } else {
+                sum -= num;
+            }
+        }
+        return sum != 0;
+    }
+}
