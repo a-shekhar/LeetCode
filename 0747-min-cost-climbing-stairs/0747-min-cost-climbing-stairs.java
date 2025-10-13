3cdeb9ed1f054cfb5e24a7651952ce91f88eedff
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-13 13:30:23 +0530

    Time: 1 ms (37.04%), Space: 43 MB (79.75%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,15 +1,29 @@
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        int[] dp = new int[n + 1];
-        if(cost.length == 1){
-            return cost[0];
+     public int minCostClimbingStairs(int[] cost) {
+
+        int[] dp = new int[cost.length+1];
+        Arrays.fill(dp, -1);
+        int startFormZero = calculate(cost, dp, 0);
+        int startFormOne = calculate(cost, dp, 1);
+        return Math.min(startFormZero, startFormOne);
+    }
+
+    private int calculate(int[] cost, int[] dp, int i) {
+        if(i >= cost.length){
+            return 0;
         }
-        dp[0] = 0;
-        dp[1] = 0;
-        for(int i = 2; i <= n; i++){
-            dp[i] = Math.min(cost[i-1] + dp[i-1], cost[i-2] + dp[i-2]);
+
+        if(dp[i] != -1){
+            return dp[i];
         }
-        return dp[n];
+
+        // count and jump 1 step
+        int val1 = cost[i] + calculate(cost, dp, i+1);
+        // count and jump 2 step
+        int val2 = cost[i] + calculate(cost, dp, i+2);
+
+        dp[i] = Math.min(val1, val2);
+
+        return dp[i];
     }
 }
