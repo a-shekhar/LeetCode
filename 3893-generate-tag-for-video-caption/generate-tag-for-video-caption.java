79a42117d82c337225cacd44b6ecfcfada2af907
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-15 20:08:34 +0530

    Time: 11 ms (37.00%) | Memory: 44.9 MB (48.62%) - LeetSync

--- 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.java
+++ 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.java
@@ -3,14 +3,10 @@ class Solution {
         if(caption.trim().length() == 0){
             return "#";
         }
-        String[] words = caption.trim().split(" ");
+        String[] words = caption.trim().split("\\s+");
         StringBuilder builder = new StringBuilder("#" + words[0].toLowerCase());
         for(int i = 1; i < words.length; i++){
-            words[i] = words[i].trim();
-             if(words[i].length() > 0)
-            builder.append(words[i].substring(0,1).toUpperCase());
-            if(words[i].length() > 1)
-            builder.append(words[i].substring(1).toLowerCase());
+            builder.append(words[i].substring(0,1).toUpperCase()).append(words[i].substring(1).toLowerCase());
         }
         builder.setLength(Math.min(builder.length(), 100));
         return builder.toString();
