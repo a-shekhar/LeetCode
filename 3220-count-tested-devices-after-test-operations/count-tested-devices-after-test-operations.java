18cbfe7c3f86d1e7ef24a4e7e1e53f1d9b8f38ce
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-19 18:25:11 +0530

    Time: 1 ms (50.75%) | Memory: 43.1 MB (74.62%) - LeetSync

--- 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.java
+++ 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.java
@@ -1,15 +1,11 @@
 class Solution {
     public int countTestedDevices(int[] batteryPercentages) {
         int testedDevice = 0;
-        int n = batteryPercentages.length;
-        for (int i = 0; i < n; i++){
-            if(batteryPercentages[i] <= 0){
-                continue;
+        
+        for (int battery : batteryPercentages){
+            if(battery - testedDevice > 0){ // since eventually it should decrement everytime one battery has been tested
+                testedDevice++;
             }
-            for(int j = i + 1; j < n; j++){
-                batteryPercentages[j]--;
-            }
-            testedDevice++;
         }
         return testedDevice;
     }
