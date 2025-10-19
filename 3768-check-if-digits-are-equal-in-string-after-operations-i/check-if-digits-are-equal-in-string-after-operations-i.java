ab5800b615eada7e6966da3c082cec75b00cbdb8
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-19 23:51:36 +0530

    Time: 11 ms (35.17%) | Memory: 44.8 MB (49.92%) - LeetSync

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.java
@@ -10,13 +10,6 @@ class Solution {
             s = builder.toString();
         }
 
-        char ch = s.charAt(0);
-        for(int i = 1; i < s.length(); i++){
-            if(s.charAt(i) != ch){
-                return false;
-            }
-        }
-
-        return true;
+        return s.charAt(0) == s.charAt(1);
     }
 }
