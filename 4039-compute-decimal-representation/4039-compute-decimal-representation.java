65734610dad8c679186f7a9d43cba4204e42815a
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-23 19:23:38 +0530

    Time: 6 ms (10.91%), Space: 43.1 MB (35.71%) - LeetHub

--- 4039-compute-decimal-representation/4039-compute-decimal-representation.java
+++ 4039-compute-decimal-representation/4039-compute-decimal-representation.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] decimalRepresentation(int n) {
         List<Integer> list = new ArrayList<>();
-        int pow = 0;
+        int place = 1;
         while(n > 0){
-            int num = (int) ((n % 10) * Math.pow(10, pow));
+            int num = (n % 10) * place;
             if(num > 0) list.add(num);
             n /= 10;
-            pow++;
+            place *= 10;
         }
         Collections.reverse(list);
         return list.stream().mapToInt(Integer::intValue).toArray();
