cf2440254ce4c5ec6de9a3b1988801db7a25d7c1
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-24 22:02:56 +0530

    Time: 60 ms (87.36%) | Memory: 41.2 MB (75.54%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] != 0){
-                    int gold  = solve(grid, n, m, i, j, 0);
+                    int gold  = solve(grid, n, m, i, j);
                     maxGold = Math.max(maxGold, gold);
                 }
             }
@@ -16,19 +16,18 @@ class Solution {
         return maxGold;
     }
 
-    private int solve(int[][] grid, int n, int m, int i, int j, int sum) {
+    private int solve(int[][] grid, int n, int m, int i, int j) {
         if(i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == 0){
             return 0;
         }
 
         int num = grid[i][j];
-        sum += num;
         grid[i][j] = 0;
 
-        int a = solve(grid, n, m, i + 1, j, sum);
-        int b = solve(grid, n, m, i - 1, j, sum);
-        int c = solve(grid, n, m, i, j + 1, sum);
-        int d = solve(grid, n, m, i, j - 1, sum);
+        int a = solve(grid, n, m, i + 1, j);
+        int b = solve(grid, n, m, i - 1, j);
+        int c = solve(grid, n, m, i, j + 1);
+        int d = solve(grid, n, m, i, j - 1);
 
         // backtracking
         grid[i][j] = num;
