521009528b43b762496b913bec65c599040b8f08
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-25 17:38:13 +0530

    Time: 1 ms (56.85%), Space: 40.2 MB (90.33%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -1,20 +1,19 @@
 class Solution {
     public int totalMoney(int n) {
         int totalMoney = 0;
-        int start = 0;
+        int initialDeposit = 0;
         int money = 0;
-        int i = 0;
-        while (i < n){
-            if(i % 7 == 0){
-                start++;
-                money = start;
-            }else{
+        for(int i = 1; i <= n; i++){
+            if(i % 7 == 1){
+                initialDeposit++;
+                money = initialDeposit;
+                totalMoney += money;
+            } else {
                 money++;
+                totalMoney += money;
             }
-            totalMoney += money;
-            i++;
         }
+
         return totalMoney;
     }
-
 }
