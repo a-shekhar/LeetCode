cbc0977004f153845fa593a554948edc8c0bc47d
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-31 20:49:30 +0530

    Time: 4 ms (10.72%), Space: 45.9 MB (7.71%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -1,14 +1,39 @@
 class Solution {
-    public int[] getSneakyNumbers(int[] nums) {
-        int[] temp = new int[nums.length-2];
-        int[] res = new int[2];
-        int index = 0;
-        for(int num : nums){
-            temp[num]++;
-            if(temp[num] == 2){
-                res[index++] = num;
+    public static  int[] getSneakyNumbers(int[] nums) {
+        int n = nums.length-2;
+        int xor = 0;
+        for(int i = 0; i < n; i++){
+            xor ^= i;
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            xor ^= nums[i];
+        }
+
+        int bitDiff = xor & -xor;
+        System.out.println(bitDiff);
+
+        int xor1 = 0;
+        int xor2 = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            int num =  bitDiff & nums[i];
+            if(num != 0){
+                xor1 ^= nums[i];
+            }else{
+                xor2 ^= nums[i];
             }
         }
-        return res;
+
+        for(int i = 0; i < n; i++){
+            int num =  bitDiff & i;
+            if(num != 0){
+                xor1 ^= i;
+            }else{
+                xor2 ^= i;
+            }
+        }
+
+        return new int[]{xor1, xor2};
     }
 }
