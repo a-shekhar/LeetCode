e7dbd6ad313fa59685814a22ab188d39df710f3d
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-10-31 23:30:55 +0530

    Time: 1 ms (81.57%), Space: 42.7 MB (5.43%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,27 +1,19 @@
 class Solution {
     public int reverse(int x) {
-        int maxInt = ((int) Math.pow(2, 31) -1)/10;
-        int minInt = ((int) Math.pow(-2, 31))/10;
-        System.out.println(maxInt);
-        int reverse = 0;
-        int rem = 0;
-        boolean isNegative =  x < 0 ? true : false;
-        if(isNegative){
-            x *= -1;
+         if(x == 0) return 0;
+        long num = 0;
+        boolean isNeg = false;
+        if(x < 0) {
+            x = -1 * x;
+            isNeg = true;
         }
         while(x > 0){
-            rem = x % 10;
-           
-            if(reverse > maxInt || reverse < minInt){
-                return 0;
-            }
-            reverse = (reverse * 10) + rem;
-           
-            x = x / 10;
+            int rem = x % 10;
+            num = (num * 10) + rem;
+            if(num < Math.pow(-2, 31)) return 0;
+            if(num > (Math.pow(2, 31) - 1)) return 0;
+            x /= 10;
         }
-        if(isNegative){
-            reverse *= -1;
-        }
-        return reverse; 
+        return isNeg ? -1 * (int) num : (int) num;
     }
 }
