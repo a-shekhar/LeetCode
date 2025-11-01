e9cbdaf6dcf1b4e3598d8ee8c2957b21ace7d36f
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-11-01 16:01:24 +0530

    Time: 17 ms (44.22%), Space: 46.3 MB (5.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,19 +1,16 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-
        int[] dp = new int[amount+1];
-
-       for(int i = 1; i <= amount; i++){
-          dp[i] = amount + 1; // to ensure it is not practical solution
-
-          for(int coin : coins){
-           
-            if (coin <= i){
-                dp[i] = Math.min(dp[i], dp[i-coin] + 1); // add current coin and check for remaining ammount
+        Arrays.fill(dp, amount + 1);  //can not have more than that
+        dp[0] = 0;
+        Arrays.sort(coins);
+        for(int i = 1; i < amount + 1; i++){
+            for(int coin : coins){
+                if(coin > i) break;
+                dp[i] = Math.min(dp[i], 1 + dp[i-coin]);
             }
-          }
-       }
-
-       return dp[amount] > amount ? -1 : dp[amount];
+        }
+        //System.out.println(Arrays.toString(dp));
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 }
