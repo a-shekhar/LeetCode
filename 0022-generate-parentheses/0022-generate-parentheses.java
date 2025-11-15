b6d25bb9ef377b384b2c5fbbd9970619c719f94d
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-11-15 19:59:39 +0530

    Time: 0 ms (100%), Space: 44.2 MB (23.62%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,31 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<>();
-        StringBuilder builder = new StringBuilder();
-        findPermutation(n, n, builder, result, 2 * n);
-        return result;
+        List<String> list = new ArrayList<>();
+        findPerm(n, n, 2 * n, list, new StringBuilder());
+        return list;
     }
 
-
-    private void findPermutation(int open, int close, StringBuilder builder, List<String> result, int n) {
+    private void findPerm(int open, int close, int n, List<String> list, StringBuilder builder){
         if(builder.length() == n){
-            result.add(builder.toString());
+            list.add(builder.toString());
             return;
         }
 
-        // add with ( bracket
         if(open > 0){
             builder.append("(");
-            findPermutation(open - 1, close, builder, result, n);
-            // delete last char
-            builder.deleteCharAt(builder.length() - 1);
+            findPerm(open - 1, close, n, list, builder);
+            builder.deleteCharAt(builder.length()-1);
         }
 
-        if (close > open) {
+        if(close > open){
             builder.append(")");
-            findPermutation(open, close - 1, builder, result, n);
-            // delete last char
-            builder.deleteCharAt(builder.length() - 1);
+            findPerm(open, close - 1, n, list, builder);
+            builder.deleteCharAt(builder.length()-1);
         }
     }
 }
