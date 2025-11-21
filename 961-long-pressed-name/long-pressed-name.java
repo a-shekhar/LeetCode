3320afe59eaadaee5bf390711c856242ed142bc6
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-11-21 20:06:39 +0530

    Time: 1 ms (81.39%) | Memory: 42.7 MB (19.29%) - LeetSync

--- 961-long-pressed-name/long-pressed-name.java
+++ 961-long-pressed-name/long-pressed-name.java
@@ -1,23 +1,18 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
-        if(name.charAt(0) != typed.charAt(0)) return false;
-        int j = 1;
-        int i = 1;
-        while(i < name.length()){
-            if(j >= typed.length()) return false;
-            if(name.charAt(i) == typed.charAt(j)){
+        
+        int j = 0;
+        int i = 0;
+        while(j < typed.length()){
+            if(i < name.length() && name.charAt(i) == typed.charAt(j)){
                 i++;
                 j++;
-            } else {
-                if(name.charAt(i-1) != typed.charAt(j)) return false;
+            } else if(j > 0 && typed.charAt(j-1) == typed.charAt(j)){
                 j++;
+            } else {
+                return false;
             }
         }
-        while(j < typed.length() && name.charAt(name.length()-1) == typed.charAt(j)) j++;
-        if(j < typed.length()) return false;
-                System.out.println(i);
-
-        if(i < name.length()) return false;
-        return true;
+        return i == name.length();
     }
 }
