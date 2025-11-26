8470f5e0e08776b29d445eae3029306c15f889de
a-shekhar <112081675+a-shekhar@users.noreply.github.com>
2025-11-26 13:18:27 +0530

    Time: 4 ms (99.72%), Space: 47.6 MB (26.17%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,7 +1,5 @@
 class Solution {
-    public boolean checkEqual(String s){
-        int i = 0;
-        int j = s.length() - 1;
+    public boolean checkEqual(String s, int i , int j){
         while(i < j){
             if(s.charAt(i++) != s.charAt(j--)) return false;
         }
@@ -9,16 +7,13 @@ class Solution {
     }
 
     public boolean validPalindrome(String s) {
-        if(checkEqual(s)) return true; 
         
         int i = 0;
         int j = s.length() - 1;
 
         while(i < j){
             if(s.charAt(i) != s.charAt(j)) {
-                String skipI = s.substring(0, i) + s.substring(i+1);
-                String skipJ = s.substring(0, j) + s.substring(j+1);        
-                return checkEqual(skipI) || checkEqual(skipJ);
+                return checkEqual(s, i+1, j) || checkEqual(s, i, j-1);
             }
             i++;
             j--;
